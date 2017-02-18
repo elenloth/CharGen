72303ecca258168962df64c347e823659389ad93
Emily <elenloth@gmail.com>
2017-02-17 21:28:46 -0500

    Update classpicker
    Updated class picker

--- class-picker.py
+++ class-picker.py
@@ -2,9 +2,15 @@
 # Emily
 # 2017-02-17
 
+import random
+
 classes=["cleric", "fighter", "rogue", "wizard"]
 
 #print classes[random (0,3)]
 
-for n in range (0, 4):
-  print classes[n]
+#for n in range (0, 4):
+#  print classes[n]
+
+randomizer = random.randint(0, 3)
+
+print "Your character class is:", classes[randomizer]
